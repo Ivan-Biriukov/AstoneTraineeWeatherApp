75d83c3b516be4a622cfaf629353718977e93d82
Ivan Biriukov <Grottt0@mail.ru>
2023-11-04 02:11:21 +0300

    Add extra assets to project; Also add added assets to Extensions; Create static UI of LaunchScreen.storyboard.

--- AstoneTraineeWeheterApp/Extensions/UIImage + Extension.swift
+++ AstoneTraineeWeheterApp/Extensions/UIImage + Extension.swift
@@ -1,9 +1,20 @@
 import UIKit.UIImage
 
 extension UIImage {
+    
     struct Common {
         static var baackground: UIImage? {
             return UIImage(named: "background")
+        }
+    }
+    
+    struct Launch {
+        static var clouds: UIImage? {
+            return UIImage(named: "Cloud")
+        }
+        
+        static var weatherCondition: UIImage? {
+            return UIImage(named: "weatherCondition")
         }
     }
 }
